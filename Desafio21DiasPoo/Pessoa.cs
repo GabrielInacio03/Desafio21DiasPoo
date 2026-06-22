5a6b1029bf52a0f6ca45084619188b00e9cffc63
GabrielInacio03 <inaciogs03@gmail.com>
2026-06-22 09:53:38 -0300

    [AULA][1 e 2] - Conceitos iniciais

---
+++ Desafio21DiasPoo/Pessoa.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Desafio21DiasPoo
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+        }
+        public int Id { get; set; } //Isso é um atributo, colocando um get e set vc transforma isso em uma propriedade
+        public string Nome { get; set; }
+        public string Endereco { get; set; }
+    }
+}
---
+++ Desafio21DiasPoo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Desafio21DiasPoo
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Pessoa gabriel = new Pessoa();
+            gabriel.Id = 1;
+            gabriel.Nome = "Gabriel";
+            gabriel.Endereco = "BRASIL SP";
+
+            Pessoa mariaJulia = new Pessoa();
+            mariaJulia.Id = 2;
+            mariaJulia.Nome = "Maria Julia";
+            mariaJulia.Endereco = "BRASIL SP";
+            Console.WriteLine("Hello World!");
+
+            Pessoa sofia = new Pessoa();
+            sofia.Id = 3;
+            sofia.Nome = "Sofia";
+            sofia.Endereco = "BRASIL SP";
+        }
+    }
+}
